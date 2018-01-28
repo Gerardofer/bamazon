6a15d2777e8ffa1bce896fbdce1bf4b257652709
Gerardo Fernandez <gerardojose13@gmail.com>
2018-01-27 20:29:07 -0800

    pushes original mysql frame

---
+++ bamazonCustomer.js
@@ -0,0 +1,10 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Alqu1m14#1",
+    database: "bamazonDB"
+});
