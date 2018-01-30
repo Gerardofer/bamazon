ad5f152052151a7ff8757f8ca43ca60094442735
Gerardo Fernandez <gerardojose13@gmail.com>
2018-01-29 23:12:28 -0800

    pushes database purchase update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,15 +1,9 @@
 var mysql = require('mysql');
+//prompts the user for input
 var inquirer = require('inquirer');
 //require Table to format the output from the Mysql database
 var Table = require("terminal-table");
-//new Table object to format table
-var t = new Table({
-    borderStyle: 2,
-    horizontalLine: true,
-    rightPadding: 0,
-    leftPadding: 1,
-    width: ["10%", "60%", "6%"]
-});
+
 //database credentials
 var connection = mysql.createConnection({
     host: "localhost",
@@ -22,24 +16,79 @@ var connection = mysql.createConnection({
 connection.connect(err => {
     if (err) throw err
 });
+
+//Run inquirer
+askItem();
+
 //quering the database to output everything in products Table
 connection.query("SELECT * FROM products", (error, results) => {
     if (error) throw error;
+    //new Table object to format table
+    var t = new Table({
+        borderStyle: 2,
+        horizontalLine: true,
+        rightPadding: 0,
+        leftPadding: 1,
+        width: ["10%", "60%", "6%"]
+    });
     //set up the table headers
     t.push(["Item ID", "Product Name", "Price"]);
     //set up the table content
     results.forEach(item => {
-        t.push([item.item_id, item.product_name, item.price])
-    })
-
-    /* Range */
+            t.push([item.item_id, item.product_name, item.price])
+        })
+        /* Range */
     t.attrRange({
         row: [0, 1]
     }, {
         align: "center",
         color: "blue"
     });
-
     console.log("" + t);
-
 });
+
+function askItem() {
+    inquirer.prompt([{
+            type: "input",
+            name: "item_id",
+            message: "Please select the Item ID you want to purchase"
+        },
+        {
+            type: "input",
+            name: "stock_quatity",
+            message: "Please select a quantity"
+        }
+    ]).then(answers => {
+        // console.log(answers.item_id);
+        connection.query("SELECT product_name, stock_quatity, price FROM products WHERE item_id =" + answers.item_id, (err, res) => {
+            if (err) {
+                console.error(message.error)
+            };
+
+            var s = new Table({
+                borderStyle: 2,
+                horizontalLine: true,
+                rightPadding: 0,
+                leftPadding: 1,
+                width: ["60%", "12%", "6%"]
+            });
+
+            s.push(["Product Name", "Order", "Price"])
+            s.push([res[0].product_name, answers.stock_quatity, res[0].price]);
+            s.attrRange({
+                row: [0, 1]
+            }, {
+                align: "center",
+                color: "blue"
+            });
+            console.log("" + s);
+
+            if ((res[0].stock_quatity - answers.stock_quatity) > 0) {
+                console.log("Your request is being fullfilled");
+                console.log("Your total is: " + (answers.stock_quatity * res[0].price));
+            } else {
+                console.log("We do not currently have enough inventory to fulfill this order")
+            }
+        })
+    })
+}
